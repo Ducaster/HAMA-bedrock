aacdb0ea232c427943e55f213b59cb99aea7ba8a
Ducaster <kooa3399@kakao.com>
2025-02-12 16:48:52 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ✅ CORS 활성화 (필요한 경우)
+  app.enableCors({});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
